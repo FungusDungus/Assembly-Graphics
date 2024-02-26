de0c92951303ef3b565bfc31cdb583cf24f35861
FungusDungus <102629338+FungusDungus@users.noreply.github.com>
2024-02-26 16:53:46 -0500

    Added Example Code, Added QOL improvements

--- code/main.asm
+++ code/main.asm
@@ -1,31 +1,53 @@
-STACK SEGMENT PARA STACK
-   DB 64 DUP (' ')
-STACK ENDS
+.model flat,stdcall
+.stack 4096
+.data
+frame db 64000 DUP(50)
 
-DATA SEGMENT 
-	MSG DB  "Hello, World!", "$"; DB: DEFINE BYTE
-								; $: terminates the string
-DATA ENDS
-
-CODE SEGMENT
-	ASSUME CS:CODE, DS:DATA 
-	; INIT SEGMENT REGS	
-	START:
-		mov ah, 00h
-      mov al, 13h
+.code
+pframe proc
+   mov ax, 0
+   mov bx, 0
+   
+   ROW:
+   COLUMN:
+   mov cx,ax
+   mov dx,bx
+   cmp ax, 320
+   inc ax
+   jb COLUMN
+   
+   
+   
+pframe endp
+; page size ? 512 Bytes?
+main proc
+      mov ah, 00h ; video mode
+      mov al, 13h ; mode
+      
       int 10h
       
-      mov ah,0ch
-      mov al,4
-      mov bh,00h
-      mov ah,0ch
-      mov cx,160
-      mov dx,120
+      PRODUCE:
+      mov ah,0ch  ; pixel mode
+      mov al,50  ; color
+      mov bh,05h  ; page #
+      mov cx,1  ; column
+      mov dx,1  ; row
       
       int 10h
-
-	STOP:
-		MOV AX, 4C00H
-		INT 10H 
-CODE ENDS 
-	END START
+      
+      ; exit on keystroke
+      mov ah,1
+      int 16h
+      jnz FIN
+      jmp PRODUCE
+      
+      FIN:
+      mov ah,0    ;clear key
+      int 16h
+        
+      mov ax,3    ;reset to text mode
+      int 10h
+      mov ah,4ch  ;exit to DOS
+      int  21h
+main endp
+end main
