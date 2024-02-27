8cc902bba984000988f42552963f451dc368ec25
FungusDungus <charles@derbes.net>
2024-02-27 00:39:05 -0500

    added a very slow screen fill function
    too many interrupts

--- code/main.asm
+++ code/main.asm
@@ -8,16 +8,26 @@ pframe proc
    mov ax, 0
    mov bx, 0
    
-   ROW:
-   COLUMN:
+   OUTERLOOP:
+   ROWPRINT:
    mov cx,ax
    mov dx,bx
-   cmp ax, 320
+   cmp cx, 320
+   ja ENDROW
    inc ax
-   jb COLUMN
-   
-   
-   
+   mov si,ax
+   mov ah, 0ch
+   mov al, 40
+   int 10h
+   mov ax,si
+   jmp ROWPRINT
+   ENDROW:
+   mov ax, 0
+   cmp dx, 200
+   inc bx
+   jb OUTERLOOP
+ 
+   ret 
 pframe endp
 ; page size ? 512 Bytes?
 main proc
@@ -26,28 +36,32 @@ main proc
       
       int 10h
       
-      PRODUCE:
-      mov ah,0ch  ; pixel mode
-      mov al,50  ; color
-      mov bh,05h  ; page #
-      mov cx,1  ; column
-      mov dx,1  ; row
       
-      int 10h
+      ;mov ah,0ch  ; pixel mode
+      ;mov al,50  ; color
+      ;mov bh,00h  ; page #
+      ;mov cx,1  ; column
+      ;mov dx,1  ; row
+      
+      ;int 10h
+      call pframe
       
       ; exit on keystroke
+      KEYSTROKE:
       mov ah,1
       int 16h
-      jnz FIN
-      jmp PRODUCE
+      jnz DONE
+      jmp KEYSTROKE
       
-      FIN:
+      DONE:
       mov ah,0    ;clear key
       int 16h
-        
+      
       mov ax,3    ;reset to text mode
       int 10h
+      
       mov ah,4ch  ;exit to DOS
       int  21h
+      
 main endp
 end main
