e16ab6bb8a0f93950e6f90fd7ab669bae15cd4ca
Tyler Turner <tturner2021@my.fit.edu>
2024-03-23 18:17:14 -0400

    WASD suppport

--- code/main.asm
+++ code/main.asm
@@ -1,246 +1,87 @@
 .model flat,stdcall
 .stack 4096
 .data
-
-; the current page counter-intuitively denotes the page not being displayed but being written to
-CurrentPage dw 0
+frame db 64000 DUP(50)
+x_pos DW 100
+y_pos DW 100
 
 .code
-
-; for convenience:
-; x, y, color
-m_vram macro x, y, color
-mov bx, x
-mov cx, y
-mov si, color
-call vram_modify
-endm
-
-; bx: x pos
-; cx: y pos
-; si: color
-; es: 0a000h, beginning of vram in memory
-vram_modify proc
-   push bx
-   mov ax, 320
-   mul cx
-   add bx, ax ; at this point the offset from es segment is in bx
-   mov es:[bx], si ; move the color
-   pop bx
-   ret
-vram_modify endp
-
-toggle_page proc
-   push ax
-   push dx
-   push cx
-   mov ax, [CurrentPage]
-   cmp ax, 0
-   je DispPage0
-   DispPage1:
-   dec [CurrentPage]
-   mov cx, 3e80h
-   jmp PerformDisp
-   DispPage0:
-   inc [CurrentPage]
-   xor cx, cx
-   PerformDisp:
-   
-   xor ax, ax
-   
-   mov dx, 3d4h
-      
-   mov ax, 0dh
-   out dx, ax
-   
-   
-   inc dx
-   mov ax, 00h; lower start bits
-   out dx,ax
-   
-   
-   dec dx
-   mov ax, 0ch
-   out dx, ax
-   
-   
-   inc dx
-   mov ax, 7dh; upper start bits
-   out dx,ax
-   
-   pop cx
-   pop dx
-   pop ax
-   ret
-   
-toggle_page endp
-
-
 main proc
-   mov ah, 0   ; video mode
-   mov al, 13h ; mode
-   int 10h
-   
-   ; preload first page
-   ; make this a procedure
-   
+    mov ah, 00h ; video mode
+    mov al, 13h ; mode
 
-   
-   mov ax, 0a000h; video memory begins at 0a000h
-   mov es, ax ; es pointer now holds page offset
-   
-   ; perform all updates to the new page here
-   
-   
-   ; tests
-   
-   mov ax, 5
-   mov bx, 60
-   mov es:[bx], ax   
-   mov ax, 90
-   mov bx, 0
-   mov es:[bx], ax
-   mov bx, 5
-   mov es:[bx], ax
- 
-   
-   mov ax,es
-   add ax, 4000
-   mov es,ax
-   
-   mov dx, 03c4h
-   mov ax, 04h
-   out dx, ax
-   
-   inc dx
-   mov ax, 06h
-   out dx, ax
-   
-   mov dx, 03d4h
-   mov ax, 14h
-   out dx, ax
-   
-   inc dx
-   mov dx, 0h
-   out dx, ax
-   
-   dec dx
-   mov ax, 17h
-   out dx,ax
-   
-   inc dx
-   mov ax, 0e3h
-   out dx, ax
-  
-   
-   mov ax, 5
-   mov bx, 60
-   mov es:[bx], ax   
-   mov ax, 90
-   mov bx, 0
-   mov es:[bx], ax
-   mov bx, 5
-   mov es:[bx], ax
-    
-   
-   mov dx, 3d4h
-      
-   mov ax, 0dh
-   out dx, ax
-   
-   
-   inc dx
-   mov ax, 80h; lower start bits
-   out dx,ax
-   
-   
-   dec dx
-   mov ax, 0ch
-   out dx, ax
-   
-   
-   inc dx
-   mov ax, 3eh; upper start bits
-   out dx,ax
-   
-   
-   
-   L1:
-   
-   ; mov bx, 0
-   ; mov cx, 0; 0 indexed
-   ; mov ax, 320
-   ; mul cx
-   ; add bx, ax
-   ; cmp [CurrentPage], 0
-   ; je Color
-   ; add bx, 64000
-   ; Color:
-   ; mov ax,90
-   ; mov es:[bx], ax
-    
-    
-   ; flip page to display the updates
-   ;the page that is being displayed is not the one being written to
-   
-   ; mov bx, 319
-   ; mov cx, 199; 0 indexed
-   ; mov ax, 320
-   ; mul cx
-   ; add bx, ax
-   ; cmp [CurrentPage], 0
-   ; je Color1
-   ; add bx, 64000
-   ; Color1:
-   ; mov ax,5
-   ; mov es:[bx], ax
+    int 10h
 
-   ;call toggle_page
-   cmp cx,50000
-   inc cx
-   jne done
-   
-   jmp L1
-   
-   ; VYSNC?
-   ; block copying
-   
- ; tests
-  ; mov si, 1
-  ; new:
-  ; mov bx, 0
-  ; mov cx, 0
-   
-   
-   ; l1:
-   ; call vram_modify
-   ; cmp bx, 320
-   ; inc bx
-   ; jb l1
-   ; xor bx, bx
-   ; cmp cx, 200
-   ; inc cx
-   ; jb l1
-   ; inc si
-   ; cmp si, 256
-   ; jbe new
-   ; mov si,1
-   ; jmp new
- ; end tests
-      
-   ; exit on keystroke
-   done:
-   mov ah,1
-   int 16h
-   
-   mov ah,0    ;clear key
-   int 16h
-   
-   mov ax,3    ;reset to text mode
-   int 10h
-   
-   mov ah,4ch  ;exit to DOS
-   int  21h
-      
-main endp
-end main
+    main_loop: 
+
+        mov ah,0ch  ; pixel mode
+        mov al,50  ; color
+        mov bh,00h  ; page #
+        mov cx, x_pos; column
+        mov dx, y_pos; row
+
+        int 10h
+
+        ; get keyboard status
+        mov ah,1 
+        int 16h
+        
+        ; if a key is pressed (ZF == 0) jump to get_keys
+        ; else jump to main_loop
+        JZ get_key
+        JMP main_loop
+
+    move_down:
+        inc y_pos
+        jmp main_loop
+
+    move_up:
+        dec y_pos
+        jmp main_loop
+
+    move_left:
+        dec x_pos
+        jmp main_loop
+    
+    move_right:
+        inc x_pos
+        jmp main_loop
+
+    get_key:
+        
+        ; get keystroke and store the ASCII value in AL
+        mov ah,0    
+        int 16h
+
+        ; if key pressed is 'w' jump to move_up
+        CMP AL, 77h
+        JE move_up
+
+        ; if key pressed is 's' jump to move_down
+        CMP al, 73h 
+        JE move_down
+
+        ; if key pressed is 'a' jump to move_left
+        CMP AL, 61h
+        JE move_left
+
+        ; if key pressed is 'd' jump to move_right
+        CMP AL, 64h
+        JE move_right
+
+        ; if key pressed is 'q' jump to done
+        CMP AL, 71h
+        JE done
+        
+        ; else jump to main_loop
+        jmp main_loop
+
+    done:      
+        mov ax,3    ;reset to text mode
+        int 10h
+
+        mov ah,4ch  ;exit to DOS
+        int  21h
+
+    main endp
+    end main
