bad65155be83a96e847f790a7585c5094c8c2c5d
FungusDungus <charles@derbes.net>
2024-03-29 22:01:40 -0400

    misery

--- code/main.asm
+++ code/main.asm
@@ -1,10 +1,11 @@
-.model flat,stdcall
 INCLUDE pacman.inc
-INCLUDE arena.inc
+.model flat,stdcall
 .stack 4096
 .data
 time_passed DB 0
+player_pos DB 0
 .code
+
 
 main proc
 
@@ -16,32 +17,55 @@ main proc
    mov ax, 0a000h; video memory begins at segment 0a000h, actual address is this X 16
    mov es, ax ; es pointer now holds page offset
    
+   GETTIME:
    
-   arenaSHOW 0,0
-   mov bx,0
    mov ah, 2ch ; system time code
-   L1:
    int 21h ; interrupt used to get system time touches cx and dx
-   cmp dl, time_passed
-   je L1
+   cmp dl, time_passed ;dl contains 1/100 of second time
    
-   mov time_passed, dl
-   add bx, 1
+   je GETTIME
    
-   ;pacmanSHOW 0, bx
-   ;pacmanSHOW 10, bx
-   cmp bx, 183
+   mov ah, 0
+   int 16h
+   cmp al, 77 ;w
+   je w
+   cmp al, 73 ;s
+   je s
+   cmp al, 61 ;a
+   je a
+   cmp al, 64 ;d
+   je d
+   cmp al, 27 ;esc
    je done
-   jmp L1
+   jmp MOVEPLAYER
+   
+   w:
+   sub player_pos, 320
+   jmp MOVEPLAYER
+   s:
+   add player_pos, 320
+   jmp MOVEPLAYER
+   a:
+   dec player_pos
+   jmp MOVEPLAYER
+   d:
+   inc player_pos
+   jmp MOVEPLAYER
+   
+   MOVEPLAYER:
+   ; mov ax, 0
+   ; mov ah, 06h
+   ; mov bh, 0
+   ; int 10h
+   ;pacmanSHOW player_x, player_y
+   mov bx, player_pos
+   mov es:[bx], BYTE PTR 32h
+   
+   jmp GETTIME
    
       
    ; exit on keystroke
    done:
-   mov ah,1
-   int 16h
-   
-   mov ah,0    ;clear key
-   int 16h
    
    mov ax,3    ;reset to text mode
    int 10h
