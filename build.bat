fd13eade2f7425694d5e5ca86dbac88e156db313
tyler <tylerturner1504@gmail.com>
2024-04-23 19:34:37 -0400

    add gitignore and logic in build and run scripts

--- build.bat
+++ build.bat
@@ -1,6 +1,18 @@
+if %1==pacman goto :pacman
+if %1==snake (goto :snake) else (echo "Improper use of build. Use ^"build pacman^" or ^"build snake^"")
+
+:pacman
 config -set cycles=100000
 cd Code
-masm main.asm;
-link main;
+masm pacman.asm;
+link pacman;
 cd ..
 config -set cycles=3000
+exit /b 0
+
+:snake
+cd Code
+masm snake.asm:
+link snake
+cd ..
+exit /b 0
--- run.bat
+++ run.bat
@@ -1,5 +1,16 @@
+if %1==pacman goto :pacman
+if %1==snake (goto :snake) else (echo "Improper use of run. Use ^"run pacman^" or ^"run snake^"")
+
+:pacman
 cd Code
 config -set cycles=50000
-main
+pacman
 config -set cycles=3000
 cd ..
+exit /b 0
+
+:snake
+cd Code
+snake
+cd ..
+exit /b 0
